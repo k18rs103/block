c04944d0ebcc9fcb0393915084047d07b1ec2cc9
k18rs103 <k18rs103@st.kyusan-u.ac.jp>
2019-11-25 13:55:43 +0900

    main.jsにncmbControllerの初期化処理

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -1,4 +1,15 @@
 // This is a JavaScript file
 
 var ncmbController = {
+  APPLICATION_KEY:"YOUR_APPLICATION_KEY",
+  CLIENT_KEY:"YOUR_CLIENT_KEY",
+
+  ncmb:null,
+  currentUser:null,
+  screenSize:null,
+  init:function(screenSize) {
+    var self = this;
+    self.ncmb = new NCMB(self.APPLICATION_KEY,self.CLIENT_KEY);
+    self.screenSize;
+  }
 }
