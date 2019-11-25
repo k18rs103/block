6c43c3af7d950b552dc8da2dfe8c3107c32f47ba
k18rs103 <k18rs103@st.kyusan-u.ac.jp>
2019-11-25 14:12:10 +0900

    スコア保存（送信）処理を実装

--- www/js/main.js
+++ www/js/main.js
@@ -268,6 +268,7 @@ var BB = {
     endGame: function() {
         BB.gameState = GAMESTATE_STOP;
         vibrate();
+        ncmbController.sendScore(BB.score);
     },
     
     // Game Clear
@@ -276,6 +277,7 @@ var BB = {
         else alert("Cleared!");
         
         BB.gameState = GAMESTATE_STOP;
+        ncmbController.sendScore(BB.score);
     }
 }
 
@@ -337,6 +339,7 @@ function init() {
     });
 
     requestAnimFrame(animate);
+    ncmbControllor.init(BB.screenSize);
 }
 
 
